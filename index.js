f51ccb54866b8e59d1a032175a00bf814f8670d9
Carmelo Bastillo <66789080+CarmeloB4@users.noreply.github.com>
2022-04-21 23:20:47 +0200

    exercise

---
+++ index.js
@@ -0,0 +1,8 @@
+// Prima di tutto occorre fare il fetch dei dati da questa api: "https://api.imgflip.com/get_memes"
+// Stai attento ad i dati che ti arrivano, ricorda che a noi servono soltanto i meme
+
+// Dobbiamo creare una funzione che, crei l'elemento ul e li, inoltre in ogni li devono essere presenti un tag p e un tag img
+// I data della card (quindi quelli dentro il tag li), devono esseri i primi 10 oggetti dentro l'array memes (che ne contiene 100)
+// Nel tag p trovermo il nome del meme, nel tag img invece l'indirizzo url
+
+// Ricordati che tutto deve essere chiamato quando facciamo l'onload della pagina (occhio a lavorare in asincrono)
