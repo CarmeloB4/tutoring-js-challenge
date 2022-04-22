42ce7e290ccf212944839a87ea5cbefb2889259e
Carmelo Bastillo <66789080+CarmeloB4@users.noreply.github.com>
2022-04-22 20:33:45 +0200

    solution

--- index.js
+++ index.js
@@ -6,3 +6,35 @@
 // Nel tag p trovermo il nome del meme, nel tag img invece l'indirizzo url
 
 // Ricordati che tutto deve essere chiamato quando facciamo l'onload della pagina (occhio a lavorare in asincrono)
+
+let data = null;
+const ul = document.createElement("ul");
+
+window.onload = async () => {
+  data = await fetchData;
+  this.createList(data);
+};
+
+const fetchData = fetch("https://api.imgflip.com/get_memes")
+  .then((response) => response.json())
+  .then((data) => data.data);
+
+function createList(memeData) {
+  const memes = memeData.memes.slice(0, 10);
+  document.getElementById("card-list").appendChild(ul);
+  memes.forEach(renderCard);
+}
+
+function renderCard(data) {
+  const li = document.createElement("li");
+  const p = document.createElement("p");
+  const img = document.createElement("img");
+
+  ul.appendChild(li);
+
+  p.innerHTML = data.name;
+  img.src = data.url;
+
+  li.appendChild(p);
+  li.appendChild(img);
+}
